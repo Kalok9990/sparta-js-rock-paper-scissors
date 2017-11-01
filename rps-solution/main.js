2ce26faf856c08dc5cc43bc8e89d197e94e74dfd
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-01 12:20:01 +0000

    all works

--- rps-solution/main.js
+++ rps-solution/main.js
@@ -24,8 +24,43 @@ function getComputerMove(computerMove){
   return computerMove || randomPlay();
 }
 
-var test = getComputerMove();
-console.log(test);
 //will need to play to 5
+function startgame(){
+  alert("Let's play Rock, Paper, Scissors!");
+
+  //score variables
+  var playerScore = 0;
+  var compScore = 0;
+
+  //play to five
+  while(playerScore < 5 && compScore < 5){
+    var playerMove = getPlayerMove();
+    var compMove = getComputerMove();
+    var winner = checkForWinner(playerMove, compMove);
+
+    if (winner === "player"){
+      playerScore ++;
+      alert("The player won that round with - " + playerMove + "against - " + compMove + "-------" + "The scores are --- Player " + playerScore + "Computer score --- " + compScore);
+    } else if (winner === "computer"){
+      compScore ++;
+      alert("The computer won that round with - " + compMove + "against - " + playerMove + "-------" + "The scores are --- Player " + playerScore + "Computer score --- " + compScore);
+    } else {
+      alert("That was a tie with - " + playerMove + compMove)
+    }
+  }
+}
 
 //check for winner
+function checkForWinner(playerMove, compMove){
+  var winner;
+  if(playerMove === compMove){
+    winner = "tie";
+  }else if((playerMove === "rock" && compMove === "scissors")||(playerMove === "paper" && compMove === "rock")||(playerMove === "scissors" && compMove === "paper")){
+    winner = "player";
+  }else{
+    winner = "computer";
+  }
+  return winner;
+}
+
+startgame();
