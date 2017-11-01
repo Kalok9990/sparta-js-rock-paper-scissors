df9c4f9db36ec45ba1abe40e4a2218f6c4afaaba
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-01 11:20:40 +0000

    randomplay works

--- rps-solution/main.js
+++ rps-solution/main.js
@@ -1,16 +1,27 @@
 //Get input
-var test = getUserInput();
-console.log(test);
-
 function getUserInput(){
   return prompt("PLease choose either rock, paper or scissors");
-
 }
 
 //Get the player move
+function getPlayerMove(){
+  return getUserInput();
+}
 
 //Random play
+function randomPlay(){
+  var randomNum = Math.floor(Math.random() * 3);
+  if(randomNum === 0){
+    return "rock";
+  }else if(randomNum === 1){
+    return "paper";
+  }else{
+    return "scissors";
+  }
+}
 
+var test = randomPlay();
+console.log(test);
 //Get the computer move
 
 //will need to play to 5
