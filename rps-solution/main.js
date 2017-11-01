a456cd6f18a089349bcb296d6e2fc4c370d6639a
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-01 11:00:12 +0000

    solution file added

---
+++ rps-solution/main.js
@@ -0,0 +1,18 @@
+//Get input
+var test = getUserInput();
+console.log(test);
+
+function getUserInput(){
+  return prompt("PLease choose either rock, paper or scissors");
+
+}
+
+//Get the player move
+
+//Random play
+
+//Get the computer move
+
+//will need to play to 5
+
+//check for winner
